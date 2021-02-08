b05a9a0b260ea74d72478980947b0fdac2cebd58
Auzet <alexpolehin28@gmail.com>
2021-02-08 09:22:51 +0300

    Added tasks

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Alexey Polehin")
+#Первый вариант
+inp = input()
+splt = inp.split(" ")
+print(splt)
+res = []
+for item in splt:
+    res.append(int(item))
+print(res[0]+res[1])
+
+#Второй вариант
+a,b = [int(item) for item in input().split(" ")]
+print (a+b)
+
+print("Hello, " + input() + "!")
