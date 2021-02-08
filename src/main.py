4c6b1963d516e4f75c70b6f790f000c2340f5d56
Danila Bludov <bludovdanila03@mail.ru>
2021-02-08 09:36:12 +0300

    first practice

--- src/main.py
+++ src/main.py
@@ -1,13 +1,1 @@
- 
-# inp = "10 3" # input() # 10 3
-# splt = inp.split(" ")
-# print(splt)
-# res = []
-# for item in splt:
-#     res.append(int(item))
-# print(res[0]+res[1])
-
-# a,b = [int(item) for item in input().split(" ")]
-# print(a + b)
-
 print ("Hello, " + input () + "!")
