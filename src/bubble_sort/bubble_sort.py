cec2d2c9abf9dbbbe99a53c93f743ddf88fb068a
alexthvest <alexthvest@gmail.com>
2021-02-13 11:31:46 +0300

    fixed: bubble sort example without tests

--- src/bubble_sort/bubble_sort.py
+++ src/bubble_sort/bubble_sort.py
@@ -1,25 +1,16 @@
-def bubble_sort(array, on_shuffle=None):
+def bubble_sort(array):
     length = len(array)
+    swapped = False
 
     for i in range(length - 1):
         for j in range(length - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+                print(' '.join(map(str, array)))
 
-    return array
-
-
-def bubble_sort_shuffles(array):
-    length = len(array)
-    shuffles = 0
-
-    for i in range(length - 1):
-        for j in range(length - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-                shuffles += 1
-
-    return shuffles
+    if not swapped:
+        print(0)
 
 
 def main():
@@ -28,9 +19,6 @@ def main():
 
     result = bubble_sort(array)
     print(" ".join(map(str, result)))
-
-    shuffles = bubble_sort_shuffles(array)
-    print(shuffles)
 
 
 if __name__ == '__main__':
--- src/bubble_sort/bubble_sort_test.py
+++ src/bubble_sort/bubble_sort_test.py
@@ -1,24 +1,11 @@
 import unittest
-from bubble_sort import bubble_sort, bubble_sort_shuffles
+from bubble_sort import bubble_sort
 
 
 class TestBubbleSort(unittest.TestCase):
 
     def test_bubble_sort(self):
-        array = [5, 3, 65, 2, 6, 4, 100]
-        expected = [2, 3, 4, 5, 6, 65, 100]
-
-        result = bubble_sort(array)
-
-        self.assertEqual(result, expected)
-
-    def test_bubble_sort_shuffles(self):
-        array = [1, 2, 3, 4, 5]
-        expected = 0
-
-        result = bubble_sort_shuffles(array)
-
-        self.assertEqual(result, expected)
+        pass
 
 
 if __name__ == '__main__':
