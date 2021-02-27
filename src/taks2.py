e29dede4f8b722f0a5e177931f5de40806af1974
Руслан Поляков <71173176+Ruslan-Polakov@users.noreply.github.com>
2021-02-27 11:49:16 +0300

    task2

--- src/taks2.py
+++ src/taks2.py
@@ -1,12 +1,14 @@
 n=int(input(" "))
 arr= [int(i)for i in input().split(" ")]
-
+c=0
 for i in range(n-1):
     for j in range(n-1):
         if arr[j+1]<arr[j]:
             arr[j],arr[j+1]=arr[j+1],arr[j]
+            c+=1
             for i in range(n):
                 print(arr[i],end=" ")
             print()
-
+if c==0:
+    print(0)
 
