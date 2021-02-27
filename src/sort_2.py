33839f085f6986acb0e6aaf9c1c927d6a8d421c4
anCreny <artem.0110@mail.ru>
2021-02-27 10:55:33 +0300

    Update sort_2.py

--- src/sort_2.py
+++ src/sort_2.py
@@ -1,19 +1,15 @@
-n = int(input())
-m = [[int(i) for i in input().split(" ")] for _ in range(n)]
+m = [[int(i) for i in input().split(" ")] for _ in range(int(input()))]
 i = 0
-c = 0 
+c = 0
 while 1:
-	if m[i][1] < m[i+1][1]:
+	if m[i][1] < m[i+1][1] or m[i][1] == m[i+1][1] and m[i][0] > m[i+1][0]:
 		m[i],m[i+1] = m[i+1],m[i]
 		c += 1
-	elif m[i][1] == m[i+1][1] and m[i][0] > m[i+1][0]:
-		m[i],m[i+1] = m[i+1],m[i]
-		c += 1
-	if c == 0 and i == n - 2:
+	if c == 0 and i == len(m) - 2:
 		break
-	elif i == n - 2:
+	elif i == len(m) - 2:
 		i = -1
 		c = 0
 	i += 1
-for i in range(n):
-	print(m[i][0],m[i][1])
+for i in range(len(m)):
+	print(m[i][0], m[i][1])
