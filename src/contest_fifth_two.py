17df01b3b759f69dcfb648121d5df2c54269a9d1
markru <mrk.ru@list.ru>
2021-03-04 00:19:02 +0300

    fix commented out code fifth task

--- src/contest_fifth_two.py
+++ src/contest_fifth_two.py
@@ -1,13 +1,3 @@
-# new_list = []
-# Quantity = int(input())
-# Arr = list(map(int,input().split(" ")))
-# for i in range(0,Quantity):
-#   if Arr[i] not in new_list:
-#     new_list.append(Arr[i])
-# Result=len(new_list)
-# print(Result)
-
-
 Quantity = int(input())
 Arr = list(map(int,input().split(" ")))
 new_list = {}
