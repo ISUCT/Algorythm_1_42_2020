f0096fa47ca0d722b2ea515972adca8554c75ed2
alexthvest <alexthvest@gmail.com>
2021-03-05 18:07:46 +0300

    edit: the 5th task rewroted without set()

--- src/module2/number_of_different.py
+++ src/module2/number_of_different.py
@@ -1,27 +1,18 @@
-# Проходит стандартные тесты, но на Яндекс.Контест проваливает 8-й тест
-def number_of_different_old(array):
-    length = -1
+def number_of_different(array):
     count = 0
 
-    for item in array:
-        if item > length:
-            length = item
+    max_item = max(array)
+    min_item = min(array)
 
-    counters = [0] * (length + 1)
-    output = [0] * len(array)
+    counters = [0] * (max_item - min_item + 1)
 
     for item in array:
-        counters[item] += 1
+        counters[item - min_item] += 1
 
     for counter in counters:
-        if counter > 0:
-            count += 1
+        count += 1 if counter > 0 else 0
 
     return count
-
-
-def number_of_different(array):
-    return len(set(array))
 
 
 def get_input_data():
@@ -42,6 +33,16 @@ def main():
     <<< 2  3  8  7  1  2  2  2  7  3  9  8  2  1  4  2  4  6  9  2
     >>> main()
     8
+
+    <<< 1
+    <<< -1
+    >>> main()
+    1
+
+    <<< 2
+    <<< -1 -2
+    >>> main()
+    2
     '''
     array, length = get_input_data()
     count = number_of_different(array)
