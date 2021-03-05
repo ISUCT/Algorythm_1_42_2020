03515b99d18fe618a87bf2b70f9a8432389a1fa6
Eugeny Konstantinov <jskonst@yandex.ru>
2021-03-05 09:10:21 +0300

    add test sample

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -1,18 +1,19 @@
+"""
+>>> import io, sys 
+>>> sys.stdin = io.StringIO(chr(10).join(['4','4 3 2 1']))  # input
+>>> bubble_sort()
+3 4 2 1
+3 2 4 1
+3 2 1 4
+2 3 1 4
+2 1 3 4
+1 2 3 4
+"""
+
 def bubble_sort():
-    """
-    >>> import io, sys 
-    >>> sys.stdin = io.StringIO(chr(10).join(['4','4 3 2 1']))  # input
-    >>> bubble_sort()
-    3 4 2 1
-    3 2 4 1
-    3 2 1 4
-    2 3 1 4
-    2 1 3 4
-    1 2 3 4
-    """
     n = input()
-    arr = input()
-    str_lst = arr.split(" ")
+    inp_string = input()
+    str_lst = inp_string.split(" ")
     res = []
     for item in str_lst:
         res.append(int(item))
