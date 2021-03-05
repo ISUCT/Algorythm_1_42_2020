aba2bad3dfc422e3586df98294cb84c78bd0a322
Creny <artem.0110@mail.ru>
2021-03-05 21:52:43 +0300

    1

--- src/sort_7.py
+++ src/sort_7.py
@@ -1,24 +1,25 @@
 A = [str(input()) for _ in range(int(input()))]
 print("Initial array:")
-for i in range(len(A)):
-    print(A[i], end=" ")
-print()
-
+for i in range(len(A)-1):
+    print(A[i], end=", ")
+print(A[len(A)-1])
+ph = 0
 m = len(A[0])
 for i in range(m-1,-1,-1):
     print('**********')
-    print("Phase",i)
+    ph += 1
+    print("Phase",ph)
     bucket = [[] for _ in range(10)]
     for j in range(len(A)):
         bucket[ord(A[j][i]) - ord('0')].append(A[j])
     for j in range(10):
         if len(bucket[j])==0:
-            print("Bucket",j,": empty")
+            print("Bucket "+str(j)+": empty")
         else:
-            print("Bucket",j,": ", end='')
-            for k in bucket[j]:
-                print(k,end=' ')
-            print()
+            print("Bucket "+str(j)+": ", end='')
+            for k in range(len(bucket[j])-1):
+                print(bucket[j][k],end=', ')
+            print(bucket[j][len(bucket[j])-1])
     p = 0
     for j in range(10):
         for k in range(len(bucket[j])):
@@ -26,6 +27,6 @@ for i in range(m-1,-1,-1):
             p += 1
 print('**********')
 print("Sorted array:")
-for i in range(len(A)):
-    print(A[i], end=" ")
-print()
+for i in range(len(A)-1):
+    print(A[i], end=", ")
+print(A[len(A)-1])
