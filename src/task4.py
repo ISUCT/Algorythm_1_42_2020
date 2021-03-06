04f94410468f4e5047fb254292eaa77740ff6ca5
Руслан Поляков <71173176+Ruslan-Polakov@users.noreply.github.com>
2021-03-06 11:02:21 +0300

    Update task4.py

--- src/task4.py
+++ src/task4.py
@@ -1,10 +1,5 @@
-input()
-
-lst = list(map(int, input().split()))
-
-input()
-
-processing = Counter(map(int, input().split()))
-
-[print('yes' if lst[i - 1] - processing[i] < 0 else 'no') for i in range(1, len(lst) + 1)]
-
+n=int(input())
+arr=[int(i)for i in input().split(" ")]
+n=int(input())
+arr1=[int(i)for i in input().split(" ")]
+print(arr,arr1)
