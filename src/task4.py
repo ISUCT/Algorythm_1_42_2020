9bb18d487c4d096c9d1e2668d0312d42c4049566
Руслан Поляков <71173176+Ruslan-Polakov@users.noreply.github.com>
2021-03-06 11:26:25 +0300

    task4

--- src/task4.py
+++ src/task4.py
@@ -1,5 +1,15 @@
-n=int(input())
-arr=[int(i)for i in input().split(" ")]
-n=int(input())
-arr1=[int(i)for i in input().split(" ")]
-print(arr,arr1)
+def m(a, b, n):
+    arr = [0]*(n+1)
+    for it in b:
+        arr[it] += 1
+    for i in range(n):
+        if a[i] >= arr[i+1]:
+            print("no")
+        else:
+            print("yes")
+G = int(input())
+U = list(map(int, input().split(" ")))
+p = int(input())
+L = list(map(int, input().split(" ")))
+m(U, L, G)
+ 
