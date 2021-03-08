63dc3ab7190a837ec75a0c31b7e2e59e169f897a
alexthvest <alexthvest@gmail.com>
2021-03-08 12:39:01 +0300

    solved: the 1st task of 3rd module

--- src/module3/substring_search.py
+++ src/module3/substring_search.py
@@ -1,21 +1,3 @@
-def hash(string, x, p):
-    '''
-    >>> hash('abc', 26, 1e9+7) == hash('abc', 26, 1e9+7)
-    True
-    >>> hash('abc', 26, 1e9+7) == hash('bca', 26, 1e9+7)
-    False
-    >>> hash('', 26, 1e9+7) == hash('', 26, 1e9+7)
-    True
-    '''
-    result = 0
-
-    for i in range(len(string)):
-        result = (result * x + ord(string[i])) % p
-
-    return result
-
-
-# Если есть % p, то выдает RE, иначе TL :(
 def find_occurences(string, substring):
     string_length = len(string)
     substring_length = len(substring)
@@ -23,21 +5,29 @@ def find_occurences(string, substring):
     if string_length < substring_length:
         return []
 
-    x = 26
-    p = 1e9 + 7
-
-    string_hash = hash(string[:substring_length], x, p)
-    substring_hash = hash(substring, x, p)
-
     occurences = []
 
+    x = 26
+    p = 101
+
+    string_hash = 0
+    substring_hash = 0
+
+    xt = 1
+
+    for i in range(substring_length - 1):
+        xt = (xt * x) % p
+
+    for i in range(substring_length):
+        string_hash = (string_hash * x + ord(string[i])) % p
+        substring_hash = (substring_hash * x + ord(substring[i])) % p
+
     for i in range(string_length - substring_length + 1):
-        if (substring_hash == string_hash) and (substring == string[i:i + substring_length]):
+        if (substring_hash == string_hash) and (substring == string[i:i+substring_length]):
             occurences.append(i)
 
         if (i + substring_length) < string_length:
-            string_hash *= x
-            string_hash -= ord(string[i]) * pow(x, substring_length)
+            string_hash = x * (string_hash - ord(string[i]) * xt)
             string_hash += ord(string[i + substring_length])
             string_hash %= p
 
