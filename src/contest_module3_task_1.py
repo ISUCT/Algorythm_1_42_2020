4f73e12f17b16f73a8c8763aedaef15078fc8909
Auzet <alexpolehin28@gmail.com>
2021-03-23 13:45:30 +0300

    task_1 update

--- src/contest_module3_task_1.py
+++ src/contest_module3_task_1.py
@@ -1,20 +1,22 @@
 def hash(A, n):
     hash_str = 0
-    for i in range(n):
-        hash_str = (hash_str * p + ord(A[i])) % q
+    m = 1
+    for i in range(n-1, -1, -1):
+        hash_str = (hash_str + ord(A[i]) * m) % q
+        m = m * p % q
     return hash_str
 
 def rabin_karp(str, sub_str, n, m):
     lst = []
-    str_hash = hash(str, n)
+    str_hash = hash(str, m)
     sub_str_hash = hash(sub_str, m)
     hash_tag = 1
-    for i in range(n):
+    for i in range(m):
         hash_tag = (hash_tag * p) % q
     for i in range(n-m+1):
         if str_hash == sub_str_hash:
             lst.append(i)
-        str_hash = (((str_hash * p - ord(str[i]) * hash_tag + ord(str[i + m - 1])) % q) + p) % p
+        str_hash = ((str_hash % q - ord(str[i - 1]) * hash_tag % q) * p % q + ord(str[i + m - 1])) % q
     return lst
 
 string = input()
