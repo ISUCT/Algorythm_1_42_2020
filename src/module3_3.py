dfa09938d2a382379e3bca87fbc1ad5017ded22d
Дмитрий Малахов <dmitriy_jg@mail.ru>
2021-05-29 16:17:02 +0300

    Contest Module 3 by Dmitriy Malakhov

---
+++ src/module3_3.py
@@ -0,0 +1,27 @@
+def pref_func(s):
+    length = len(s)
+    Meter = [0] * length    
+    Meter[0] = 0
+    for i in range (length - 1):
+        j = Meter[i]
+        while (j > 0) and (s[i + 1] != s[j]):
+            j = Meter[j - 1]
+        if (s[i + 1] == s[j]):
+            Meter[i+1] = j + 1
+        else:
+            Meter[i + 1] = 0
+    return Meter
+
+text = str(input())
+s = list(text)
+
+length = len(s)
+pref = pref_func(s)
+
+for i in range(length - 1, length):
+    result = length - pref[i]
+
+if (length % result == 0):
+    print(length // result)
+else:
+    print('1') 
